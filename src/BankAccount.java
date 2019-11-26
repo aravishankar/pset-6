def757feec90812aa2b8a10f1d544b067a0c19df
aravishankar <aryanravishankar02@gmail.com>
2019-11-26 16:10:52 -0500

    Commit 21

--- src/BankAccount.java
+++ src/BankAccount.java
@@ -1,9 +1,7 @@
 import java.text.NumberFormat;
 
 public class BankAccount {
-        
-private static long prevAccountNo = 100000000L;
-    
+
     private int pin;
     private long accountNo;
     private double balance;
@@ -33,7 +31,7 @@ private static long prevAccountNo = 100000000L;
 
     public String getBalance() {
         NumberFormat currency = NumberFormat.getCurrencyInstance();
-        
+
         return currency.format(balance);
     }
 
@@ -43,14 +41,15 @@ private static long prevAccountNo = 100000000L;
 
     public int deposit(double amount) {
         if (amount <= 0) {
-            return ATM.INVALID;    
+            return ATM.INVALID;
+        } else if ((amount + balance) > 999999999999.99){
+        	return ATM.OVERFLOW;
         } else {
             balance = balance + amount;
         }
-            
+
         return ATM.SUCCESS;
     }
-
 
     public int withdraw(double amount) {
         if (amount <= 0) {
@@ -60,35 +59,33 @@ private static long prevAccountNo = 100000000L;
         } else {
             balance = balance - amount;
         }
-        
+
         return ATM.SUCCESS;
     }
 
-    
-    
     ////////////////////////////////////////////////////////////////////////////
     //                                                                        //
     // Refer to the Simple ATM tutorial to fill in the details of this class. //
     //                                                                        //
     ////////////////////////////////////////////////////////////////////////////
-    
+
     /*
      * Formats the account balance in preparation to be written to the data file.
-     * 
+     *
      * @return a fixed-width string in line with the data file specifications.
      */
-    
+
     private String formatBalance() {
-        return String.format("%1$15s", balance);
+        return String.format("%.2f", balance);
     }
-    
+
     /*
      * Converts this BankAccount object to a string of text in preparation to
      * be written to the data file.
-     * 
+     *
      * @return a string of text formatted for the data file
      */
-    
+
     @Override
     public String toString() {
         return String.valueOf(accountNo) +
@@ -96,4 +93,5 @@ private static long prevAccountNo = 100000000L;
             accountHolder.serialize() +
             formatBalance();
     }
+
 }
